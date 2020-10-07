3b80eadc0f51232cd36929702d36171b9671d8e5
Anupam Shankar.A <anupams@qburst.com>
2020-10-07 22:46:55 +0530

    test

--- contactus/settings.py
+++ contactus/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,5 +123,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
 STATIC_ROOT = Path.joinpath(BASE_DIR, 'contact/staticfiles')
 STATIC_URL = 'contact/static/'
+
+django_heroku.settings(locals())
